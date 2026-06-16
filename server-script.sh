c523543d91612fbabea2be1b035960270044f088
menghieng-sorn <menghiengsorn.it@gmail.com>
2026-06-16 22:39:09 +0700

    sshagent

--- server-script.sh
+++ server-script.sh
@@ -1,4 +1,4 @@
-sudo yum install java -y
+sudo yum install java-21-amazon-corretto-devel -y
 sudo yum install git -y
 sudo yum install maven -y
 # sudo yum install docker -y
@@ -9,9 +9,9 @@ if [ -d "addressbook-v1" ]
 then
   echo "repo is cloned and exists"
   cd /home/ec2-user/addressbook-v1
-  git pull origin master
+  git pull origin dev
 else
-  git clone https://github.com/preethid/addressbook-v1.git
+  git clone https://github.com/menghieng-sorn/addressbook-v1.git
 fi
 
 cd /home/ec2-user/addressbook-v1
